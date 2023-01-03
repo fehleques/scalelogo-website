b1b136a0d2b0eab15ba86af6116aa550b9f6f6be
fehleques <fehleques@gmail.com>
2023-01-03 16:02:54 -0300

    Added popup functionalities

--- main.js
+++ main.js
@@ -7,6 +7,43 @@ $(document).ready(function () {
         }, 7000);
     }
 });
+//When X Button is clicked the modal close
+var hide = document.getElementsByClassName('popup_close-btn');
+var show = document.getElementsByClassName('popup_discount-wrapper');
+for (var i = 0; i < hide.length; i++) {
+    hide[i].addEventListener('click', function () {
+        for (var j = 0; j < show.length; j++) {
+            show[j].style.display = 'none';
+        }
+    });
+}
+
+//New date
+var date = new Date();
+date.setHours(date.getHours() + 17);
+var dateString = date.toLocaleString();
+var dateElement = document.getElementsByClassName('sl_popup-card-date')[0];
+dateElement.innerHTML = dateString;
+
+
+//Replace text "Dribbble" for "Copied"
+$('.popup_code-value').click(function () {
+    $(this).text('copied');
+    setTimeout(function () {
+        $(this).text('dribbble');
+    }, 2000);
+});
+
+var dribbble = document.getElementsByClassName('popup_code-value');
+for (var i = 0; i < dribbble.length; i++) {
+    dribbble[i].addEventListener('click', function () {
+        var textArea = document.createElement('textarea');
+        textArea.value = 'dribbble';
+        document.body.appendChild(textArea);
+        textArea.select();
+        document.execCommand('copy');
+    });
+}
 //Videoask lazy load
 $(window).on("load", function () {
     $(".sl_hero-right-wrapper").css({ display: "block", transition: "all 300ms ease-in-out", transform: "translateX(0)" });
