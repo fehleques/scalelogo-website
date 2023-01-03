9a280ec43519db3aa2e87a2f97fd3df22b113bf3
fehleques <fehleques@gmail.com>
2023-01-03 15:05:40 -0300

    Comments in the main.js file

--- main.js
+++ main.js
@@ -1,29 +1,38 @@
+//Videoask lazy load
 $(window).on("load", function () {
     $(".sl_hero-right-wrapper").css({ display: "block", transition: "all 300ms ease-in-out", transform: "translateX(0)" });
-}),
-    $(window).scroll(function () {
-        $(window).scrollTop() > 100 ? $(".sl_navbar").addClass("on--scroll") : $(".sl_navbar").removeClass("on--scroll");
-    }),
-    $(".sl_nav-mobile-menu-wrapper").on("click", function () {
-        $(".sl_nav-wrapper").toggleClass("is--open");
-    }),
-    $(".sl_nav-link, .sl_main-button, .sl_nav-mobile-menu-wrapper").on("click", function () {
-        $(this).closest(".sl_nav-wrapper").removeClass("is--open");
-    }),
-    $("#t-gallery-button").on("click", function () {
-        $(".sl_work-comp").toggleClass("is--expanded");
-    }),
-    $(".sl_expand-btn.is--testimonials").on("click", function () {
-        $(".sl_testimonials-comp").toggleClass("is--expanded");
-    }),
-    $("#t-gallery-button").click(function () {
-        $(this).text("Expand to see all" == $(this).text() ? "Okay, I got it" : "Expand to see all");
-    }),
-    $(".sl_expand-btn.is--testimonials").click(function () {
-        $(this).text("See all recent reviews" == $(this).text() ? "Okay, I got it" : "See all recent reviews");
-    }),
-    $(document).ready(function () {
-        $(".sl_faq-item").click(function () {
-            $(".sl_faq-item").removeClass("faq-clicked"), $(this).addClass("faq-clicked");
-        });
+});
+//Navbar background change after scroll
+$(window).scroll(function () {
+    $(window).scrollTop() > 100 ? $(".sl_navbar").addClass("on--scroll") : $(".sl_navbar").removeClass("on--scroll");
+});
+//Mobile menu open nav menu
+$(".sl_nav-mobile-menu-wrapper").on("click", function () {
+    $(".sl_nav-wrapper").toggleClass("is--open");
+});
+//Elements inside nav menu close menu after click
+$(".sl_nav-link, .sl_main-button, .sl_nav-mobile-menu-wrapper").on("click", function () {
+    $(this).closest(".sl_nav-wrapper").removeClass("is--open");
+});
+//Logo gallery button change comp height on click
+$("#t-gallery-button").on("click", function () {
+    $(".sl_work-comp").toggleClass("is--expanded");
+});
+//Logo gallery toggle text on click
+$("#t-gallery-button").click(function () {
+    $(this).text("Expand to see all" == $(this).text() ? "Okay, I got it" : "Expand to see all");
+});
+//Testimonials button change comp height on click
+$(".sl_expand-btn.is--testimonials").on("click", function () {
+    $(".sl_testimonials-comp").toggleClass("is--expanded");
+});
+//Testimonials button toggle text on click
+$(".sl_expand-btn.is--testimonials").click(function () {
+    $(this).text("See all recent reviews" == $(this).text() ? "Okay, I got it" : "See all recent reviews");
+});
+//FAQ open on click
+$(document).ready(function () {
+    $(".sl_faq-item").click(function () {
+        $(".sl_faq-item").removeClass("faq-clicked"), $(this).addClass("faq-clicked");
     });
+});
